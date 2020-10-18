837dae3927b58c31fe6655496b60ad2be33033ba
Dena Neuman <denaweiss5@gmail.com>
2020-10-18 00:19:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,20 @@
-# require modules here
+require 'yaml'
 
-def load_library
+
+def load_library(file_path)
+emoticons= YAML.load_file("./lib/emoticons.yml")
+results={}
+emoticons.each  do |name, emoticon_array|
+  results[name]={english: emoticon_array[0], japanese: emoticon_array[1]}
+end
+results
+
+end
+
+def get_japanese_emoticon(file_path,emoticon)
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path,emoticon)
+emoticon_hash = load_library(file_path)
 end
