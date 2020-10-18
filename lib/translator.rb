33f005429991af8ac03edaf6819ee4efdbf75811
Dena Neuman <denaweiss5@gmail.com>
2020-10-18 00:28:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_english_meaning(file_path,(＾ｖ＾))
 emoticon_hash = load_library(file_path)
 english_results={}
 emoticon_hash.each do |name, japanese_emoticon|
-  english_results[japanese_emoticon]=(english:)
+  english_results[japanese_emoticon]={english:}
 end
 english_results
 
