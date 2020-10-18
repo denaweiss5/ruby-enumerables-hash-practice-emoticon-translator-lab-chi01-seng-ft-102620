ddbc4e68e7c7986dc3f6bc9aebd0b52671b3cccc
Dena Neuman <denaweiss5@gmail.com>
2020-10-18 01:25:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,8 @@ end
 def get_japanese_emoticon(file_path, emoticon)
 emoticon_hash = load_library(file_path)
 emoticon_hash.each do|name, emoticons|
+  if emoticon == emoticons[:japanese]
+    return emoticons[:japanese]
   binding.pry
 end
 end
